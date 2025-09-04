8463dd4f0aab1846ae7d3316aaf71e3b1c29f808
amandabongiovani6-ops <amandabongiovani6@gmail.com>
2025-09-04 19:27:21 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -25,8 +25,30 @@ self.addEventListener("activate", event => {
   self.clients.claim();
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+self.addEventListener("fetch", (event) => {
+  if (event.request.mode === "navigate") {
+  
+    
+    event.respondWith(
+      fetch(event.request).catch(() =>
+        caches.match(event.request).then((resp) => resp || caches.match())
+      )
+    );
+  } else {
+   
+
+    event.respondWith(
+      caches.match(event.request).then((resp) => {
+        return (
+          resp ||
+          fetch(event.request).then((response) => {
+            return caches.open(CACHE_NAME).then((cache) => {
+              cache.put(event.request, response.clone());
+              return response;
+            });
+          })
+        );
+      })
+    );
+  }
 });
